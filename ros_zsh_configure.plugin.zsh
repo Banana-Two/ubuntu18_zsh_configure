4a4b7fa38b82dbff29d1b1af1fefb1168001b1fb
Banana-Two <62785154+Banana-Two@users.noreply.github.com>
2021-07-20 09:05:22 +0800

    Update ros_zsh_configure.plugin.zsh

--- ros_zsh_configure.plugin.zsh
+++ ros_zsh_configure.plugin.zsh
@@ -7,3 +7,9 @@ source /home/banana/study_ws/devel/setup.zsh
 #ifconfig查看你的电脑ip地址
 export ROS_HOSTNAME=192.168.3.3
 export ROS_MASTER_URI=http://${ROS_HOSTNAME}:11311
+
+#Set Java-13
+export JAVA_HOME=/usr/lib/jvm/jdk-13
+export CLASSPATH=.:${JAVA_HOME}/lib
+export PATH=${JAVA_HOME}/bin:$PATH
+

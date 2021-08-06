e9a5294841e40411b2dcada1f2bba17782daabe1
Banana-Two <62785154+Banana-Two@users.noreply.github.com>
2021-08-07 06:19:09 +0800

    Update ros_zsh_configure.plugin.zsh

--- ros_zsh_configure.plugin.zsh
+++ ros_zsh_configure.plugin.zsh
@@ -23,7 +23,7 @@ alias firefox='firefox &'
 alias gdb='gdb -q'
 
 #Set CUDA
-export PATH=/usr/local/cuda-11.4/bin${PATH:+:${PATH}}
-export LD_LIBRARY_PATH=/usr/local/cuda-11.4/lib64${LD_LIBRARY_PATH:+:${LD_LIBRARY_PATH}}
+export PATH=/usr/local/cuda/bin${PATH:+:${PATH}}
+export LD_LIBRARY_PATH=/usr/local/cuda/lib64${LD_LIBRARY_PATH:+:${LD_LIBRARY_PATH}}
 
 

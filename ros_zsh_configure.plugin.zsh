2f7eece0395752d7849b7544a337ac7e9d776aa7
Banana-Two <62785154+Banana-Two@users.noreply.github.com>
2021-08-06 09:41:32 +0800

    Update ros_zsh_configure.plugin.zsh

--- ros_zsh_configure.plugin.zsh
+++ ros_zsh_configure.plugin.zsh
@@ -24,5 +24,4 @@ alias gdb='gdb -q'
 
 #Set CUDA
 export PATH=/usr/local/cuda-11.4/bin${PATH:+:${PATH}}
-export LD_LIBRARY_PATH=/usr/local/cuda-11.4/lib64\
-                         ${LD_LIBRARY_PATH:+:${LD_LIBRARY_PATH}}
+export LD_LIBRARY_PATH=/usr/local/cuda-11.4/lib64${LD_LIBRARY_PATH:+:${LD_LIBRARY_PATH}}

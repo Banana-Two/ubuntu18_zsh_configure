1104fafc3c025d1d9c1ee712972829367f7f3f40
Banana-Two <62785154+Banana-Two@users.noreply.github.com>
2021-08-30 17:32:15 +0800

    Update ros_zsh_configure.plugin.zsh

--- ros_zsh_configure.plugin.zsh
+++ ros_zsh_configure.plugin.zsh
@@ -9,9 +9,9 @@ export ROS_HOSTNAME=192.168.3.3
 export ROS_MASTER_URI=http://${ROS_HOSTNAME}:11311
 
 #Set Java-13
-export JAVA_HOME=/usr/lib/jvm/jdk-13
-export CLASSPATH=.:${JAVA_HOME}/lib
-export PATH=${JAVA_HOME}/bin:$PATH
+#export JAVA_HOME=/usr/lib/jvm/jdk-13
+#export CLASSPATH=.:${JAVA_HOME}/lib
+#export PATH=${JAVA_HOME}/bin:$PATH
 
 #Set MATLAB
 alias matlab='/home/banana/Polyspace/R2021a/bin/matlab >/dev/null 2>&1 &'

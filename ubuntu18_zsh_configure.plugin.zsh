be6217723870729b9db442279c1d6e3fffb867f7
Liang <62785154+Banana-Two@users.noreply.github.com>
2021-09-12 15:50:30 +0800

    Update and rename ros_zsh_configure.plugin.zsh to ubuntu18_zsh_configure.plugin.zsh

--- ubuntu18_zsh_configure.plugin.zsh
+++ ubuntu18_zsh_configure.plugin.zsh
@@ -2,33 +2,37 @@
 source /opt/ros/melodic/setup.zsh
 source /home/banana/catkin_ws/devel/setup.zsh
 source /home/banana/study_ws/devel/setup.zsh
-
 # Set ROS Network
 #ifconfig查看你的电脑ip地址
 export ROS_HOSTNAME=192.168.3.3
 export ROS_MASTER_URI=http://${ROS_HOSTNAME}:11311
 
-#Set Java-13
-#export JAVA_HOME=/usr/lib/jvm/jdk-13
-#export CLASSPATH=.:${JAVA_HOME}/lib
-#export PATH=${JAVA_HOME}/bin:$PATH
+
 
 #Set MATLAB
-alias matlab='/home/banana/Polyspace/R2021a/bin/matlab >/dev/null 2>&1 &'
+#alias matlab='/home/banana/Polyspace/R2021a/bin/matlab >/dev/null 2>&1 &'
+
+
 
 #Set firefox
 alias firefox='firefox >/dev/null 2>&1 &'
 
+
+
 #Set gdb
 alias gdb='gdb -q'
+
+
 
 #Set CUDA
 export PATH=/usr/local/cuda/bin${PATH:+:${PATH}}
 export LD_LIBRARY_PATH=/usr/local/cuda/lib64${LD_LIBRARY_PATH:+:${LD_LIBRARY_PATH}}
 
 
+
 #Set pycharm
-alias pycharm='/snap/pycharm-professional/254/bin/pycharm.sh >/dev/null 2>&1 &'
+#alias pycharm='/snap/pycharm-professional/254/bin/pycharm.sh >/dev/null 2>&1 &'
+
 
 
 #Set gvim
@@ -38,11 +42,6 @@ alias gvim='gvim -p'
 
 #Set PDF
 alias pdf='/home/banana/Downloads/program/PDF-Studio-11/start &'
-
-
-
-# set wine
-# export WINEARCH=win32
 
 
 

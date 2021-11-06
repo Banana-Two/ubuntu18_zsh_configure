b6fed5136bcec3f3bc4dae4a1cc3f4936ffe4686
Banana-Two <1184903633@qq.com>
2021-11-06 16:03:15 +0800

    comment

--- ubuntu18_zsh_configure.plugin.zsh
+++ ubuntu18_zsh_configure.plugin.zsh
@@ -1,11 +1,11 @@
 # Set ROS melodic
-source /opt/ros/melodic/setup.zsh
-source /home/banana/catkin_ws/devel/setup.zsh
-source /home/banana/study_ws/devel/setup.zsh
+# source /opt/ros/melodic/setup.zsh
+# source /home/banana/catkin_ws/devel/setup.zsh
+# source /home/banana/study_ws/devel/setup.zsh
 # Set ROS Network
 #ifconfig查看你的电脑ip地址
-export ROS_HOSTNAME=192.168.3.3
-export ROS_MASTER_URI=http://${ROS_HOSTNAME}:11311
+# export ROS_HOSTNAME=192.168.3.3
+# export ROS_MASTER_URI=http://${ROS_HOSTNAME}:11311
 
 
 

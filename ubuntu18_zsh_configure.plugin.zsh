6ace19d9b310e04c6a5b9e3dfb06889f2842de37
Banana-Two <1184903633@qq.com>
2021-11-07 20:00:23 +0800

    uncomment

--- ubuntu18_zsh_configure.plugin.zsh
+++ ubuntu18_zsh_configure.plugin.zsh
@@ -1,7 +1,7 @@
 # Set ROS melodic
-# source /opt/ros/melodic/setup.zsh
-# source /home/banana/catkin_ws/devel/setup.zsh
-# source /home/banana/study_ws/devel/setup.zsh
+source /opt/ros/melodic/setup.zsh
+source /home/banana/catkin_ws/devel/setup.zsh
+source /home/banana/study_ws/devel/setup.zsh
 # Set ROS Network
 #ifconfig查看你的电脑ip地址
 # export ROS_HOSTNAME=192.168.3.3
@@ -9,41 +9,41 @@
 
 
 
-#Set MATLAB
-#alias matlab='/home/banana/Polyspace/R2021a/bin/matlab >/dev/null 2>&1 &'
+# Set MATLAB
+# alias matlab='/home/banana/Polyspace/R2021a/bin/matlab >/dev/null 2>&1 &'
 
 
 
-#Set firefox
+# Set firefox
 alias firefox='firefox >/dev/null 2>&1 &'
 
 
 
-#Set gdb
+# Set gdb
 alias gdb='gdb -q'
 
 
 
-#Set CUDA
+# Set CUDA
 export PATH=/usr/local/cuda/bin${PATH:+:${PATH}}
 export LD_LIBRARY_PATH=/usr/local/cuda/lib64${LD_LIBRARY_PATH:+:${LD_LIBRARY_PATH}}
 
 
 
-#Set pycharm
-#alias pycharm='/snap/pycharm-professional/254/bin/pycharm.sh >/dev/null 2>&1 &'
+# Set pycharm
+# alias pycharm='/snap/pycharm-professional/254/bin/pycharm.sh >/dev/null 2>&1 &'
 
 
 
-#Set vim and gvim
+# Set vim and gvim
 alias gvim='gvim -p'
 alias vim='vim -p'
 alias vi='vim -p'
 
-#Set PDF
+# Set PDF
 alias pdf='/home/banana/Downloads/program/PDF-Studio-11/start &'
 
 
 
-# set pip
+# Set pip
 alias pip='python3 -m pip'
